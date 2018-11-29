7f1635a0898a5a870c886606ee6d73a741192ea6
Yamei <guillaume.kleinpoort@gmail.com>
2018-11-29 10:11:06 -0500

    init test

---
+++ src/api/API.java
@@ -0,0 +1,16 @@
+package api;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api/")
+public class API extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add( APIPage.class );
+        return h;
+    }
+}
